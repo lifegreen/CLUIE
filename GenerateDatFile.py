9220b50aa7ed1716ee6f9574ed67123915b974db
Mark Fomenko <lifegreen.m@gmail.com>
2022-05-10 14:23:16 +0100

    Implement specifying dest and locFile arguments

--- GenerateDatFile.py
+++ GenerateDatFile.py
@@ -2,26 +2,42 @@ import os
 import sys
 import argparse
 import re
+import pandas as pd
 
 
 def GenerateDatFile(screen, locFile=None, outPath=None):
-	IDs, limits = getRange(getStringIDs(screen))
+	strings = None
+	if locFile:
+		# TODO: Deal with bad lines
+		strings = pd.read_table(locFile
+								, encoding='utf-16'
+								, delimiter='\t'
+								, index_col=0
+								, names=['ID', 'String']
+								, on_bad_lines='skip'
+								)
+
+	# Process the screen file
+	IDs, limits = getRange(getStringIDs(screen, strings))
 
 	name = os.path.splitext(os.path.basename(args.path))[0]
 	path = f"Text.UI.{name}.dat"
 
+	# Output the dat file in the current directory if no output path is specified
 	if outPath: path = os.path.join(outPath, path)
 
 	with open(path, 'w') as file:
 		file.write(datFileHeader(name, limits))
 
 		file.write(f"rangestart {limits[0]} {limits[1]}\n")
+
 		for ID in IDs:
-			if locFile:
+			if strings:
 				if ID in strings.index:	file.write(f"{ID}\t{strings.loc[ID].item()}\n")
 				else:					file.write(f"{ID}\t${ID} - Doesn't exist\n")
 			else:
 				file.write(f"{ID}\t${ID}\n")
+
 		file.write("rangeend\n")
 
 
@@ -80,17 +96,33 @@ if __name__ == "__main__":
 						help='Destination for the .dat file',
 						metavar='outPath')
 
+	parser.add_argument('-l', '--locFile',
+						help='File containing original strings',
+						metavar='locFile')
+
 	args = parser.parse_args()
 
 
+	# Validate parameters
+	if args.dest and not os.path.isdir(args.dest):
+		print(f"[Error] Is not a directory: {args.dest}")
+		sys.exit()
+
+	if args.locFile:
+		if not os.path.isfile(args.locFile):
+			print(f'[Error] File doesn\'t exist: {args.locFile}')
+			sys.exit()
+
+		elif not args.locFile.endswith('.ucs'):
+			print(f'[Error] Not a localisation (.ucs) file: {args.locFile}')
+			sys.exit()
+
 
 	if os.path.isfile(args.path):
-		GenerateDatFile(args.path)
-
-		# [print(i) for i in getStringIDs(args.path)]
+		GenerateDatFile(args.path, args.locFile, args.dest)
 
 	elif os.path.isdir(args.path):
-		print("DIR")
+		print('DIR')
 
 	else:
 	    print('The path specified does not exist')
